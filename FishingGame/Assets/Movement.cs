db52163dbef58db0823b758ccc1ad1c47bf7401f
Cameron burridge <cmburridge4022@gmail.com>
2020-03-29 21:43:23 -0600

    Added boat movement

---
+++ FishingGame/Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public Vector3 position;
+	public CharacterController controller;
+	public float speed = 1f;
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+		position.x = speed * Input.GetAxis("Horizontal");
+		controller.Move(position * Time.deltaTime);
+		position.y = speed * Input.GetAxis("Vertical");
+	}
+}
