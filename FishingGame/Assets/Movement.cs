0c702b8fc3250802d32e0676bf8f112c34028c71
Cameron burridge <cmburridge4022@gmail.com>
2020-03-29 23:22:05 -0600

    Did tutorial to remake movement to add animation.

--- FishingGame/Assets/Movement.cs
+++ FishingGame/Assets/Movement.cs
@@ -1,29 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
 
-	public Vector3 position;
-	public CharacterController controller;
-	public float speed = 1f;
+	public float moveSpeed = 5f;
+	
+	public Rigidbody2D rb;
 	public Animator animator;
-	void Start () {
-		
-	}
 	
-	void Update ()
+	private Vector2 movement;
+
+	private void Update()
 	{
-		position.x = speed * Input.GetAxis("Horizontal");
-		controller.Move(position * Time.deltaTime);
-		position.y = speed * Input.GetAxis("Vertical");
-
-		if (Input.GetButtonDown("Down"))
-		{
-			animator.SetBool("IsDown", true);
-		}
+		movement.x = Input.GetAxisRaw("Horizontal");
+		movement.y = Input.GetAxisRaw("Vertical");
+		
+		animator.SetFloat("Horizontal", movement.x);
+		animator.SetFloat("Vertical", movement.y);
+		animator.SetFloat("Speed", movement.sqrMagnitude);
 	}
 
-	
+	private void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+	}
 }
